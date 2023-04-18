0caeb144d5df427f718080e6536b308df4b6105a
Gerardo Daniel Montes Hernández <gdmontes@Fonatur.gob.mx>
2023-04-18 13:20:36 -0500

    adding logs

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -5,6 +5,8 @@ import com.fundamentos.springboot.fundamentos.been.MyBeanWithDependency;
 import com.fundamentos.springboot.fundamentos.been.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
 import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
+	private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
 	private MyBean myBean;
 	private ComponentDependency componentDependency;
 	private MyBeanWithDependency myBeanWithDependency;
@@ -44,5 +47,7 @@ public class FundamentosApplication implements CommandLineRunner {
 		System.out.println(myBeanWithProperties.function());
 
 		System.out.println(userPojo.getEmail() + "-" + userPojo.getPassword());
+
+		LOGGER.error("Esto es un error generado a partir de la aplicacion");
 	}
 }
--- src/main/java/com/fundamentos/springboot/fundamentos/been/MyBeanWithDependencyImplement.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/been/MyBeanWithDependencyImplement.java
@@ -1,7 +1,11 @@
 package com.fundamentos.springboot.fundamentos.been;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
 
+    Log LOGGER = LogFactory.getLog(MyBeanWithDependencyImplement.class);
     MyOperation myOperation;
     public MyBeanWithDependencyImplement(MyOperation myOperation) {
         this.myOperation = myOperation;
@@ -9,7 +13,12 @@ public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
 
     @Override
     public void printWithDependency() {
+        LOGGER.info("Hemos ingresado al metodo printWithDependecy");
+
         int numero = 1;
+
+        LOGGER.debug("El numero enviado como parametro a la dependencia operation es: " + numero);
+
         System.out.println(myOperation.sum(numero));
         System.out.println("Hola desde la implementación de un bean con dependencia");
     }
